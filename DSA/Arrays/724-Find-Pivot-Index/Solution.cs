2c79c4ee3e1ab98254164b6c6c39d450dc37fc42
Sravanthi <sravanthiravipati25@gmail.com>
2026-08-09 16:02:13 +0530

    Add solution for finding pivot index in an array
    Implemented a solution to find the pivot index in an array, where the sum of the elements on the left equals the sum of the elements on the right.

--- DSA/Arrays/724-Find-Pivot-Index/Solution.cs
+++ DSA/Arrays/724-Find-Pivot-Index/Solution.cs
@@ -1,1 +1,35 @@
+// LeetCode 724 - Find Pivot Index
+// https://leetcode.com/problems/find-pivot-index/
+//
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
+public class Solution
+{
+    public int PivotIndex(int[] nums)
+    {
+        int totalSum = 0;
+
+        // Calculate total sum
+        foreach (int num in nums)
+        {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if (leftSum == rightSum)
+            {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
