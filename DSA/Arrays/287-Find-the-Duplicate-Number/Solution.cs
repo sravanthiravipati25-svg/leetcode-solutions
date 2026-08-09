08ef4a90ee1052c775c7b56b52e504148287ccac
Sravanthi <sravanthiravipati25@gmail.com>
2026-08-09 16:01:07 +0530

    Add solution for finding the duplicate number
    Implemented Floyd's Tortoise and Hare algorithm to find the duplicate number in an array.

--- DSA/Arrays/287-Find-the-Duplicate-Number/Solution.cs
+++ DSA/Arrays/287-Find-the-Duplicate-Number/Solution.cs
@@ -1,1 +1,33 @@
+// LeetCode 287 - Find the Duplicate Number
+// https://leetcode.com/problems/find-the-duplicate-number/
+//
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
+public class Solution
+{
+    public int FindDuplicate(int[] nums)
+    {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Find intersection point
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        while (slow != fast);
+
+        // Find entrance of the cycle
+        slow = nums[0];
+
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
